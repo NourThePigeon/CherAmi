fd117b9aa3093b14f2546e395bf9e807379e08d0
NourAl-Zaghloul <38329890+NourAl-Zaghloul@users.noreply.github.com>
2018-07-27 11:42:14 -0400

    Adding basic structure

--- R/pigeon_classifyr.R
+++ R/pigeon_classifyr.R
@@ -6,6 +6,21 @@
 ##$    it can do very rudimentary cleaning as well (converting characters to NULL)
 
 pigeon_classify <- function(x, null_values = NULL){
-
+  
+  Strtype <- sapply(x, class)
+  
+  null_values <- c(null_values, "", " ", "-", "_", "NA", "NA_real_", "NA_complex_", "NA_character_", "NA_integer",
+                   "na", "n/a", "N/A", "Na", "null", "Null", "unknown", "Unknown", "UNKNOWN", "?")
+  null_values <- unique(null_values)
+  
+  x[x %in% null_values] <- NULL
+  
+  for(i in seq(Strtype)){
+    
+    if(Strtype[i] == "character"){
+    
+    } else { next }
+      
+    }
 
 }

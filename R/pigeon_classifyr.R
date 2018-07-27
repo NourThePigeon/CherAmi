b1aed60841d09501b2d7033995d5861f4b3cb7d2
NourAl-Zaghloul <NourAlZaghloul@gmail.com>
2018-07-26 22:21:12 -0400

    adding classifyr bones

---
+++ R/pigeon_classifyr.R
@@ -0,0 +1,11 @@
+## classifyr
+##
+##$ The point of pigeon_classify() is to quickly look over the columns in a df
+##$    and convert any columns incorrectly typed "character" to "numeric".
+##$    This is planned to happen after datacleaning steps already occur, but
+##$    it can do very rudimentary cleaning as well (converting characters to NULL)
+
+pigeon_classify <- function(x, null_values = NULL){
+
+
+}

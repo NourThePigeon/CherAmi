a31ee1939da6e3f9593d80397fc5f8f014724162
NourAl-Zaghloul <NourAlZaghloul@gmail.com>
2018-11-01 14:45:44 -0400

    exploreR: Basic 2variable plots
    Added in the ability to create xy plots (plus conditional options for sleeker UI).
    Next: grouping options

--- R/pigeon_explorer.R
+++ R/pigeon_explorer.R
@@ -18,21 +18,37 @@ ui <- fluidPage(
                   "text/comma-separated-values,text/plain",
                   ".csv")
       ),
-      tags$hr(),
-
-      selectInput(inputId = "plotchoice",
-                  label = "Choose your plot",
-                  choices = c("Density", "Histogram", "Rug")),
-      selectInput(inputId = "xchoice",
-                  label = "Choose your x variable",
-                  choices = c("names()")),
-
       checkboxInput(inputId = "header",
                     label = "Header",
                     value = TRUE),
       checkboxInput(inputId = "head",
                     label = "Head",
-                    value = FALSE)
+                    value = FALSE),
+      tags$hr(),
+
+
+      selectInput(inputId = "var_quant",
+                  label = "How many variables?",
+                  choices = 1:3),
+      conditionalPanel(
+        condition = "input.var_quant == 1",
+        selectInput(inputId = "q1_plot",
+                    label = "Choose your plot",
+                    choices = c("Density", "Histogram", "Rug"))
+      ),
+      conditionalPanel(
+        condition = "input.var_quant == 2",
+        selectInput(inputId = "q2_plot",
+                    label = "Choose your plot",
+                    choices = c("Box", "Scatter", "Bar", "Violin"))
+      ),
+
+      uiOutput("xchoice"),
+      conditionalPanel(
+        condition = "input.var_quant == 2",
+        uiOutput("ychoice")
+      )
+
     ),
     mainPanel(
       tabsetPanel(
@@ -50,8 +66,8 @@ server <- function(input, output) {
 
     req(input$file)
 
-    rawdf <- read.csv(input$file$datapath,
-                       header = input$header)
+    rawdf <- as.data.frame(read.csv(input$file$datapath,
+                                    header = input$header))
 
     if(input$head){
       return(head(rawdf))
@@ -61,23 +77,62 @@ server <- function(input, output) {
 
   })
 
+  output$xchoice <- renderUI({
+    req(input$file)
+
+    rawdf <- as.data.frame(read.csv(input$file$datapath,
+                                    header = input$header))
+
+    var_names <- names(rawdf)
+
+    selectInput(inputId = "xchoice",
+                label = "Choose x variable",
+                choices = var_names)
+  })
+
+  output$ychoice <- renderUI({
+    req(input$file)
+
+    rawdf <- as.data.frame(read.csv(input$file$datapath,
+                                    header = input$header))
+
+    var_names <- names(rawdf)
+
+    selectInput(inputId = "ychoice",
+                label = "Choose y variable",
+                choices = var_names)
+  })
+
   output$plot <- renderPlot({
     req(input$file)
 
-    rawdf <- read.csv(input$file$datapath,
-                      header = input$header)
+    rawdf <- as.data.frame(read.csv(input$file$datapath,
+                      header = input$header))
 
-    if(input$plotchoice == "Density"){
-      geomgraph <- geom_density()
-    } else if(input$plotchoice == "Histogram"){
-      geomgraph <- geom_histogram()
-    } else if (input$plotchoice == "Rug"){
-      geomgraph <- geom_rug()
+    if(input$var_quant == 1){
+      if(input$q1_plot == "Density"){
+        geomgraph <- geom_density()
+      } else if(input$q1_plot == "Histogram"){
+        geomgraph <- geom_histogram()
+      } else if (input$q1_plot == "Rug"){
+        geomgraph <- geom_rug()
+      }
+      dfplot <- ggplot(rawdf, aes(x = rawdf[,input$xchoice])) +
+        geomgraph
+    } else if (input$var_quant == 2){
+      if(input$q2_plot == "Box"){
+        geomgraph <- geom_boxplot()
+      } else if(input$q2_plot == "Scatter"){
+        geomgraph <- geom_jitter()
+      } else if (input$q2_plot == "Bar"){
+        geomgraph <- geom_bar()
+      } else if (input$q2_plot == "Violin"){
+        geomgraph <- geom_violin()
+      }
+        dfplot <- ggplot(rawdf, aes(x = rawdf[,input$xchoice], y = rawdf[,input$ychoice])) +
+          geomgraph
+
     }
-
-
-    dfplot <- ggplot(rawdf, aes(x = rawdf[,1])) +
-    geomgraph
 
     return(dfplot)
 

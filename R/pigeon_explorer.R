db396e2f0002983f470dbd6af9fe77341cc3afa3
NourAl-Zaghloul <NourAlZaghloul@gmail.com>
2018-11-13 11:42:12 -0500

    WIP

--- R/pigeon_explorer.R
+++ R/pigeon_explorer.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(shiny)
 library(devtools)
 
+### UI ####
 ui <- fluidPage(
 
   titlePanel(
@@ -11,70 +12,117 @@ ui <- fluidPage(
 
   sidebarLayout(
     sidebarPanel(
-      fileInput(inputId = "file",
-                label = "Choose CSV File",
-                accept = c(
-                  "text/csv",
-                  "text/comma-separated-values,text/plain",
-                  ".csv")
-      ),
-      checkboxInput(inputId = "header",
-                    label = "Header",
-                    value = TRUE),
-      checkboxInput(inputId = "head",
-                    label = "Head",
-                    value = FALSE),
-      tags$hr(),
 
+      tabsetPanel(
+        type = 'tabs',
+        tabPanel("Data",
 
-      selectInput(inputId = "var_quant",
-                  label = "How many variables?",
-                  choices = 1:3),
-      conditionalPanel(
-        condition = "input.var_quant == 1",
-        selectInput(inputId = "q1_plot",
-                    label = "Choose your plot",
-                    choices = c("Density", "Histogram", "Rug"))
-      ),
-      conditionalPanel(
-        condition = "input.var_quant == 2",
-        selectInput(inputId = "q2_plot",
-                    label = "Choose your plot",
-                    choices = c("Box", "Scatter", "Bar", "Violin"))
-      ),
+                 fileInput(inputId = "file",
+                           label = "Choose CSV File",
+                           accept = c(
+                             "text/csv",
+                             "text/comma-separated-values,text/plain",
+                             ".csv")),
+                 checkboxInput(inputId = "header",
+                               label = "Header",
+                               value = TRUE),
+                 checkboxInput(inputId = "head",
+                               label = "Head",
+                               value = FALSE),
+                 tags$hr(),
+                 selectInput(inputId = "var_quant",
+                             label = "How many variables?",
+                             choices = 1:3),
+                 uiOutput("xchoice"),
+                 conditionalPanel(
+                   condition = "input.var_quant >= 2",
+                   uiOutput("ychoice")),
+                 conditionalPanel(
+                   condition = "input.var_quant == 3",
+                   uiOutput("zchoice"))
 
+                 ),
 
-      uiOutput("xchoice"),
-      conditionalPanel(
-        condition = "input.var_quant == 2",
-        uiOutput("ychoice")
-      ),
-      radioButtons(inputId = "aes_include",
-                   label = "Grouping?",
-                   choices = c("Yes", "No"),
-                   selected = "No"),
-      conditionalPanel(
-        condition = "input.aes_include == 'Yes'",
-        selectInput(inputId = "aestype",
-                    label = "What type of grouping?",
-                    choices = c("Color", "Fill", "Size", "Shape", "Linetype", "Facet")),
-        uiOutput("aeschoice")
+        #TODO
+        # tabPanel("Plots",
+        #          uiOutput("mytabs")),
+
+        tabPanel("P1",
+                 conditionalPanel(
+                   condition = "input.var_quant == 1",
+                   selectInput(inputId = "q1p1",
+                               label = "Choose your plot",
+                               choices = c("Density", "Histogram", "Rug"),
+                               selected = NULL)),
+                 conditionalPanel(
+                   condition = "input.var_quant == 2",
+                   selectInput(inputId = "q2p1",
+                               label = "Choose your plot",
+                               choices = c("Box", "Scatter", "Bar", "Violin"),
+                               selected = NULL))
+                 ),
+
+        tabPanel("P2",
+                 conditionalPanel(
+                   condition = "input.var_quant == 1",
+                   selectInput(inputId = "q1p2",
+                               label = "Choose your plot",
+                               choices = c("Density", "Histogram", "Rug"),
+                               selected = NULL)),
+                 conditionalPanel(
+                   condition = "input.var_quant == 2",
+                   selectInput(inputId = "q2p2",
+                               label = "Choose your plot",
+                               choices = c("Box", "Scatter", "Bar", "Violin"),
+                               selected = NULL))
+                 ),
+
+        tabPanel("P3",
+                 conditionalPanel(
+                   condition = "input.var_quant == 1",
+                   selectInput(inputId = "q1p3",
+                               label = "Choose your plot",
+                               choices = c("Density", "Histogram", "Rug"),
+                               selected = NULL)),
+                 conditionalPanel(
+                   condition = "input.var_quant == 2",
+                   selectInput(inputId = "q2p3",
+                               label = "Choose your plot",
+                               choices = c("Box", "Scatter", "Bar", "Violin"),
+                               selected = NULL))
+                 ),
+
+        tabPanel("Aes",
+                 radioButtons(inputId = "aes_include",
+                              label = "Grouping?",
+                              choices = c("Yes", "No"),
+                              selected = "No"),
+                 conditionalPanel(
+                   condition = "input.aes_include == 'Yes'",
+                   selectInput(inputId = "aestype",
+                               label = "What type of grouping?",
+                               choices = c("Color", "Fill", "Size", "Shape", "Linetype", "Facet")),
+                   uiOutput("aeschoice"))
+        )
       )
 
     ),
+    ### Main Panel ####
     mainPanel(
       tabsetPanel(
         type = "tabs",
-        tabPanel("Plot", plotOutput("plot")),
         tabPanel("Table", tableOutput("table")),
+        tabPanel("Plot", plotOutput("plot")),
         tabPanel("R code", verbatimTextOutput("ggcode"))
       )
     )
   )
 )
 
+### Server ####
 server <- function(input, output) {
 
+  ### Reactive functions ####
   rawdf <- reactive({
     req(input$file)
     as.data.frame(read.csv(input$file$datapath,
@@ -89,23 +137,7 @@ server <- function(input, output) {
 
   })
 
-  #### Attempt to fix #####
-  # aes_var <- reactive({
-  #   if (input$aes_include == "Yes"){
-  #     input$aestype
-  #   }
-  # })
-
-  output$table <- renderTable({
-    req(input$file)
-
-    if(input$head){
-      return(head(rawdf()))
-    }else{
-      return(rawdf())
-    }
-
-  })
+  ### Var UI Outputs ####
 
   output$xchoice <- renderUI({
     req(input$file)
@@ -123,6 +155,14 @@ server <- function(input, output) {
                 choices = var_names())
   })
 
+  output$zchoice <- renderUI({
+    req(input$file)
+
+    selectInput(inputId = "zchoice",
+                label = "Choose z variable",
+                choices = var_names())
+  })
+
   output$aeschoice <- renderUI({
     req(input$file)
 
@@ -131,87 +171,113 @@ server <- function(input, output) {
                 choices = var_names())
   })
 
+  #### Plot UI Outputs ####
+
+  #TODO
+
+  output$densityUI <- renderUI({})
+
+  output$histogramUI <- renderUI({})
+
+  output$rugUI <- renderUI({})
+
+
+  ### Main Table Outputs ####
+
+  output$table <- renderTable({
+    req(input$file)
+
+    if(input$head){
+      return(head(rawdf()))
+    }else{
+      return(rawdf())
+    }
+
+  })
+
   output$plot <- renderPlot({
 
     if (input$var_quant == 1){
-      if (input$q1_plot == "Density"){
-        geomgraph <- geom_density()
-      } else if (input$q1_plot == "Histogram"){
-        geomgraph <- geom_histogram()
-      } else if (input$q1_plot == "Rug"){
-        geomgraph <- geom_rug()
+
+      if (input$q1p1 == "Density"){
+        plot1 <- geom_density()
+      } else if (input$q1p1 == "Histogram"){
+        plot1 <- geom_histogram()
+      } else if (input$q1p1 == "Rug"){
+        plot1 <- geom_rug()
       }
 
       if (input$aes_include == "Yes"){
-        ## Uses a reactive as a function argument. Does not work
-        # dfplot <- ggplot(rawdf(), aes(x = rawdf()[,input$xchoice], aes_var() = rawdf()[,input$aeschoice])) +
-        #   geomgraph
+
         if (input$aestype == "Color"){
           dfplot <- ggplot(rawdf(), aes(x = rawdf()[,input$xchoice], color = rawdf()[,input$aeschoice])) +
-            geomgraph
+            plot1
         } else if (input$aestype == "Fill"){
           dfplot <- ggplot(rawdf(), aes(x = rawdf()[,input$xchoice], fill = rawdf()[,input$aeschoice])) +
-            geomgraph
+            plot1
         } else if (input$aestype == "Size"){
           dfplot <- ggplot(rawdf(), aes(x = rawdf()[,input$xchoice], size = rawdf()[,input$aeschoice])) +
-            geomgraph
+            plot1
         } else if (input$aestype == "Shape"){
           dfplot <- ggplot(rawdf(), aes(x = rawdf()[,input$xchoice], shape = rawdf()[,input$aeschoice])) +
-            geomgraph
+            plot1
         } else if (input$aestype == "Linetype"){
           dfplot <- ggplot(rawdf(), aes(x = rawdf()[,input$xchoice], linetype = rawdf()[,input$aeschoice])) +
-            geomgraph
+            plot1
         } else if (input$aestype == "Facet"){
           dfplot <- ggplot(rawdf(), aes(x = rawdf()[,input$xchoice])) +
-            geomgraph +
+            plot1 +
             facet_grid(row = input$aeschoice)
         }
       } else {
         dfplot <- ggplot(rawdf(), aes(x = rawdf()[,input$xchoice])) +
-          geomgraph
+          plot1
       }
 
 
     } else if (input$var_quant == 2){
-      if(input$q2_plot == "Box"){
-        geomgraph <- geom_boxplot()
-      } else if(input$q2_plot == "Scatter"){
-        geomgraph <- geom_jitter()
-      } else if (input$q2_plot == "Bar"){
-        geomgraph <- geom_bar()
-      } else if (input$q2_plot == "Violin"){
-        geomgraph <- geom_violin()
+      if(input$q2p1 == "Box"){
+        plot1 <- geom_boxplot()
+      } else if(input$q2p1 == "Scatter"){
+        plot1 <- geom_jitter()
+      } else if (input$q2p1 == "Bar"){
+        plot1 <- geom_bar()
+      } else if (input$q2p1 == "Violin"){
+        plot1 <- geom_violin()
       }
 
+      # if(input$q2p2 == "Box"){
+      #   plot2 <- geom_boxplot()
+      # } else if(input$q2p2 == "Scatter"){
+      #   plot2 <- geom_jitter()
+      # } else if (input$q2p2 == "Bar"){
+      #   plot2 <- geom_bar()
+      # } else if (input$q2p2 == "Violin"){
+      #   plot2 <- geom_violin()
+      # }
+
       if(input$aes_include == "Yes"){
-        ## Uses a reactive as a function argument. Does not work
-        # dfplot <- ggplot(rawdf(), aes(x = rawdf()[,input$xchoice], y = rawdf()[,input$ychoice], aes_var() = rawdf()[,input$aeschoice])) +
-        #   geomgraph
+
         if (input$aestype == "Color"){
-          dfplot <- ggplot(rawdf(), aes(x = rawdf()[,input$xchoice], y = rawdf()[,input$ychoice], color = rawdf()[,input$aeschoice])) +
-            geomgraph
+          dfplot <- ggplot(rawdf(), aes(x = rawdf()[,input$xchoice], y = rawdf()[,input$ychoice], color = rawdf()[,input$aeschoice]))
         } else if (input$aestype == "Fill") {
-          dfplot <- ggplot(rawdf(), aes(x = rawdf()[,input$xchoice], y = rawdf()[,input$ychoice], fill = rawdf()[,input$aeschoice])) +
-            geomgraph
+          dfplot <- ggplot(rawdf(), aes(x = rawdf()[,input$xchoice], y = rawdf()[,input$ychoice], fill = rawdf()[,input$aeschoice]))
         } else if (input$aestype == "Size") {
-          dfplot <- ggplot(rawdf(), aes(x = rawdf()[,input$xchoice], y = rawdf()[,input$ychoice], size = rawdf()[,input$aeschoice])) +
-            geomgraph
+          dfplot <- ggplot(rawdf(), aes(x = rawdf()[,input$xchoice], y = rawdf()[,input$ychoice], size = rawdf()[,input$aeschoice]))
         } else if (input$aestype == "Shape") {
-          dfplot <- ggplot(rawdf(), aes(x = rawdf()[,input$xchoice], y = rawdf()[,input$ychoice], shape = rawdf()[,input$aeschoice])) +
-            geomgraph
+          dfplot <- ggplot(rawdf(), aes(x = rawdf()[,input$xchoice], y = rawdf()[,input$ychoice], shape = rawdf()[,input$aeschoice]))
         } else if (input$aestype == "Linetype") {
-          dfplot <- ggplot(rawdf(), aes(x = rawdf()[,input$xchoice], y = rawdf()[,input$ychoice], linetype = rawdf()[,input$aeschoice])) +
-            geomgraph
+          dfplot <- ggplot(rawdf(), aes(x = rawdf()[,input$xchoice], y = rawdf()[,input$ychoice], linetype = rawdf()[,input$aeschoice]))
         } else if (input$aestype == "Facet") {
           dfplot <- ggplot(rawdf(), aes(x = rawdf()[,input$xchoice], y = rawdf()[,input$ychoice])) +
-            geomgraph +
             facet_grid(row = input$aeschoice)
         }
       } else {
-        dfplot <- ggplot(rawdf(), aes(x = rawdf()[,input$xchoice], y = rawdf()[,input$ychoice])) +
-          geomgraph
+        dfplot <- ggplot(rawdf(), aes(x = rawdf()[,input$xchoice], y = rawdf()[,input$ychoice]))
       }
     }
+
+    dfplot <- dfplot + plot1
 
     return(dfplot)
 
@@ -222,4 +288,5 @@ server <- function(input, output) {
   })
 }
 
+### Shiny App####
 shinyApp(ui = ui, server = server)
